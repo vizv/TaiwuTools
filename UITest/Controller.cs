4e8d3c15ae37c41ab080e4f1a52bb9c31fb0c1ed
Wenxuan Zhao <viz@linux.com>
2020-02-19 20:10:21 -0800

    WIP

---
+++ UITest/Controller.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UITest
+{
+    class Controller : MonoBehaviour
+    {
+        //private static Controller Instance;
+        private static GameObject GameObjectInstance;
+
+        void Awake()
+        {
+            Main.Logger.Log("Controller#Awake()");
+            SceneManager.sceneLoaded += onSceneLoaded;
+        }
+
+        private void onSceneLoaded(Scene scene, LoadSceneMode _mode)
+        {
+            Main.Logger.Log($"Controller#onSceneLoaded({scene.name})");
+        }
+
+        void Update()
+        {
+            // FIXME: DEBUG
+            if (Input.GetKeyDown("delete"))
+            {
+
+                //Main.Logger.Log("All Scenes:");
+                //for (var i = 0; i < SceneManager.sceneCount; i++)
+                //{
+                //    var scene = SceneManager.GetSceneAt(i);
+                //    Main.Logger.Log(scene.name);
+                //}
+                //var activeScene = SceneManager.GetActiveScene();
+                //Main.Logger.Log("Active Scene:" + activeScene.name);
+                //Main.Logger.Log("Game Objects:");
+
+                //foreach (var go in activeScene.GetRootGameObjects())
+                //{
+                //    dumpGameObject(go);
+                //}
+
+                var defaultScene = GameObjectInstance.gameObject.scene;
+                foreach (var go in defaultScene.GetRootGameObjects())
+                {
+                    dumpGameObject(go);
+                }
+
+                //GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+                //foreach (GameObject go in allObjects)
+                //{
+                //    if (go.activeInHierarchy)
+                //    {
+                //        Main.Logger.Log("=====");
+                //        // dumpGameObject(go);
+                //        // Main.Logger.Log($"{go.name}({go.transform.name}) -> {go.transform.parent.gameObject.name}");
+                //        Main.Logger.Log($"[{go.scene.name}#{go.scene.GetHashCode()}] {go.transform} -> {go.transform.parent}");
+                //    }
+                //}
+            }
+        }
+
+        private static void dumpGameObject(GameObject gameObject, int level = 0)
+        {
+            var prefix = string.Concat(Enumerable.Repeat("  ", level)) + "- ";
+            Main.Logger.Log($"{prefix}+{gameObject.name}");
+
+            foreach (Component component in gameObject.GetComponents<Component>())
+            {
+                dumpComponent(component, level + 1);
+            }
+
+            foreach (Transform child in gameObject.transform)
+            {
+                dumpGameObject(child.gameObject, level + 1);
+            }
+        }
+
+        private static void dumpComponent(Component component, int level = 0)
+        {
+            var prefix = string.Concat(Enumerable.Repeat("  ", level)) + "- ";
+            var name = component == null ? "(null)" : component.GetType().Name;
+            Main.Logger.Log($"{prefix}{name}");
+        }
+
+        internal static void Load()
+        {
+            Main.Logger.Log("Controller.Load()");
+            if (GameObjectInstance == null)
+            {
+                Main.Logger.Log("Controller.Load() -> new Controller()");
+                GameObjectInstance = new GameObject("ControllerGameObject");
+                GameObjectInstance.AddComponent(typeof(Controller));
+                DontDestroyOnLoad(GameObjectInstance);
+            }
+        }
+    }
+}
---
+++ UITest/UITest.cs
@@ -0,0 +1,23 @@
+﻿using UnityModManagerNet;
+using static UnityModManagerNet.UnityModManager;
+
+namespace UITest
+{
+    public static class Main
+    {
+        public static ModEntry Mod;
+        public static ModEntry.ModLogger Logger
+        {
+            get { return Mod?.Logger; }
+        }
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Mod = modEntry;
+            Logger.Log("Hello UITest");
+            Controller.Load();
+
+            return true;
+        }
+    }
+}
