38563fc438093199433cbae0e3d6137a87e1f3a8
Wenxuan Zhao <viz@linux.com>
2020-02-20 18:22:54 -0800

    Get inspector working

--- UITest/Controller.cs
+++ UITest/Controller.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace UITest
 {
@@ -9,11 +12,97 @@ namespace UITest
     {
         //private static Controller Instance;
         private static GameObject GameObjectInstance;
+        private string debugText;
 
         void Awake()
         {
             Main.Logger.Log("Controller#Awake()");
             SceneManager.sceneLoaded += onSceneLoaded;
+        }
+
+        bool left;
+        bool bottom;
+
+        void OnGUI()
+        {
+            //var positions = "";
+            //var allGameObjects = FindObjectsOfType<GameObject>().Where(gameObject => gameObject.activeInHierarchy);
+            //foreach (var go in allGameObjects)
+            //{
+            //    positions += $"{String.Join("|", go.GetComponents<Component>().Select(comp => comp.GetType().Name).ToArray())}|";
+            //    //positions += $"{go.transform?.position.x},{go.transform?.position.y}|";
+            //}
+
+            var screenWidth = (float)Screen.width;
+            var screenHeight = (float)Screen.height;
+            var position = Input.mousePosition;
+
+            if (position.x < screenWidth / 4) left = true;
+            if (screenWidth - position.x < screenWidth / 4) left = false;
+            if (position.y < screenHeight / 3) bottom = true;
+            if (screenHeight - position.y < screenHeight / 3) bottom = false;
+
+            GUIStyle boxStyle = new GUIStyle("box");
+            boxStyle.normal.textColor = Color.black;
+            boxStyle.normal.background = Texture2D.whiteTexture;
+            boxStyle.fixedWidth = screenWidth / 4;
+            boxStyle.fixedHeight = screenHeight / 3;
+            boxStyle.alignment = TextAnchor.UpperRight;
+
+            GUIStyle labelStyle = new GUIStyle("label");
+            labelStyle.normal.textColor = Color.blue;
+
+            GUIStyle resultStyle = new GUIStyle("label");
+            resultStyle.normal.textColor = Color.red;
+
+            PointerEventData pointerData = new PointerEventData(EventSystem.current)
+            {
+                pointerId = -1,
+            };
+
+            pointerData.position = Input.mousePosition;
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            //Vector2 pos = Camera.current.ScreenToWorldPoint(Input.mousePosition);
+
+            //RaycastHit2D[] hits = Physics2D.RaycastAll(pos, new Vector2(0, 0), Mathf.Infinity);
+            //var output = "miss";
+            //if (hits.Length > 0) output = "" + hits.Length;
+
+            GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+            GUILayout.BeginHorizontal();
+            if (left) GUILayout.FlexibleSpace();
+            GUILayout.BeginVertical();
+            if (!bottom) GUILayout.FlexibleSpace();
+            GUILayout.BeginVertical("DEBUG", boxStyle);
+            GUILayout.Label("Mouse Position:", labelStyle);
+            GUILayout.Label(position.ToString(), labelStyle);
+            GUILayout.Label("Game Object Under Mouse:", labelStyle);
+            //GUILayout.Label($"{allGameObjects.Count()}", labelStyle);
+            GUILayout.Label(results.Count() + "", labelStyle);
+            foreach (var result in results)
+            {
+                GUILayout.Label($"- /{dump(result.gameObject.transform)}", resultStyle);
+            }
+            //GUILayout.Label(pos.ToString(), labelStyle);
+            //GUILayout.Label(output, labelStyle);
+            //GUILayout.Label(positions, labelStyle);
+            GUILayout.EndVertical();
+            if (bottom) GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+            if (!left) GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.EndArea();
+        }
+
+        private static string dump(Transform tf)
+        {
+            if (tf.parent != null) {
+                return $"{dump(tf.parent)}/{tf.name}";
+            }
+            return tf.name;
         }
 
         private void onSceneLoaded(Scene scene, LoadSceneMode _mode)
@@ -26,38 +115,25 @@ namespace UITest
             // FIXME: DEBUG
             if (Input.GetKeyDown("delete"))
             {
-
-                //Main.Logger.Log("All Scenes:");
-                //for (var i = 0; i < SceneManager.sceneCount; i++)
-                //{
-                //    var scene = SceneManager.GetSceneAt(i);
-                //    Main.Logger.Log(scene.name);
-                //}
-                //var activeScene = SceneManager.GetActiveScene();
-                //Main.Logger.Log("Active Scene:" + activeScene.name);
-                //Main.Logger.Log("Game Objects:");
-
-                //foreach (var go in activeScene.GetRootGameObjects())
+                // DEBUG - dump all game objects
+                //var defaultScene = gameObject.scene;
+                //foreach (var go in defaultScene.GetRootGameObjects())
                 //{
                 //    dumpGameObject(go);
                 //}
 
-                var defaultScene = GameObjectInstance.gameObject.scene;
-                foreach (var go in defaultScene.GetRootGameObjects())
-                {
-                    dumpGameObject(go);
-                }
-
                 //GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
-                //foreach (GameObject go in allObjects)
+
+                //Ray ray = Camera.current.ScreenPointToRay(Input.mousePosition);
+                //RaycastHit hit;
+
+                //if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~5))
                 //{
-                //    if (go.activeInHierarchy)
-                //    {
-                //        Main.Logger.Log("=====");
-                //        // dumpGameObject(go);
-                //        // Main.Logger.Log($"{go.name}({go.transform.name}) -> {go.transform.parent.gameObject.name}");
-                //        Main.Logger.Log($"[{go.scene.name}#{go.scene.GetHashCode()}] {go.transform} -> {go.transform.parent}");
-                //    }
+                //    Main.Logger.Log(hit.transform.gameObject.name);
+                //}
+                //else
+                //{
+                //    Main.Logger.Log($"ray: {ray}");
                 //}
             }
         }
@@ -65,7 +141,7 @@ namespace UITest
         private static void dumpGameObject(GameObject gameObject, int level = 0)
         {
             var prefix = string.Concat(Enumerable.Repeat("  ", level)) + "- ";
-            Main.Logger.Log($"{prefix}+{gameObject.name}");
+            Main.Logger.Log($"{prefix}+{gameObject.name}@{gameObject.transform.position}-{gameObject.layer}");
 
             foreach (Component component in gameObject.GetComponents<Component>())
             {
@@ -91,8 +167,8 @@ namespace UITest
             if (GameObjectInstance == null)
             {
                 Main.Logger.Log("Controller.Load() -> new Controller()");
-                GameObjectInstance = new GameObject("ControllerGameObject");
-                GameObjectInstance.AddComponent(typeof(Controller));
+                GameObjectInstance = new GameObject("ControllerGameObject", typeof(Controller));
+                // GameObjectInstance.AddComponent(typeof(Controller));
                 DontDestroyOnLoad(GameObjectInstance);
             }
         }
--- UITest/UITest.cs
+++ UITest/UITest.cs
@@ -1,23 +1,47 @@
-﻿using UnityModManagerNet;
+﻿using UnityEngine;
+using UnityModManagerNet;
 using static UnityModManagerNet.UnityModManager;
 
 namespace UITest
 {
-    public static class Main
+    public class Main : MonoBehaviour
     {
         public static ModEntry Mod;
         public static ModEntry.ModLogger Logger
         {
             get { return Mod?.Logger; }
         }
+        public static bool Enabled
+        {
+            get { return Mod == null ? false : Mod.Enabled; }
+        }
 
-        public static bool Load(UnityModManager.ModEntry modEntry)
+        public static bool Load(ModEntry modEntry)
         {
             Mod = modEntry;
+            Mod.OnToggle = OnToggle;
             Logger.Log("Hello UITest");
-            Controller.Load();
 
             return true;
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Logger.Log($"Current: {Enabled}");
+            Logger.Log($"Toggle: {value}");
+            Logger.Log($"Camera.allCameras.Length = {Camera.allCameras.Length}");
+            // if (value) Controller.Load();
+            new GameObject(typeof(Main).FullName, typeof(Main));
+            var GameObjectInstance = new GameObject("ControllerGameObject", typeof(Controller));
+            DontDestroyOnLoad(GameObjectInstance);
+
+            return true;
+        }
+
+        void Awake()
+        {
+            Logger.Log("Main#Awake()");
+            DontDestroyOnLoad(this);
         }
     }
 }
