bef82a8f2dcc4f40d38e6ae2ac766cfb8382b03e
Wenxuan Zhao <viz@linux.com>
2020-03-07 18:27:10 -0800

    Add TaiwuInspector project layout

---
+++ TaiwuInspector/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿// This file is part of the TaiwuTools <https://github.com/vizv/TaiwuTools/>.
+// Copyright (C) 2020  Taiwu Modding Community Members
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("TaiwuInspector")]
+[assembly: AssemblyDescription("太吾绘卷检视器")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Taiwu Modding Community")]
+[assembly: AssemblyProduct("TaiwuInspector")]
+[assembly: AssemblyCopyright("Copyright © Taiwu Modding Community Members 2020")]
+[assembly: AssemblyCulture("zh-Hans")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
